85ead8d3daa2e91fc5893d55802eae48697aafb8
Rohan sinha <2003rohansinha@gmail.com>
2025-04-28 01:38:30 +0530

    Fix syntax and logic errors in multiple C++ files including Queue/Dequeue.cpp and nonperindome.cpp

--- Queue/Dequeue.cpp
+++ Queue/Dequeue.cpp
@@ -1,4 +1,4 @@
-#include<iostream.
+#include<iostream>
 #include<string>
 #include<queue>
 
@@ -7,117 +7,122 @@ using namespace std;
 class node
 {
     public:
-    int data
-    Node * next *prev;
+    int data;
+    node *next;
+    node *prev;
 
     node(int value)
     {
-        data=value;
-        next null;
-        prev = null;
-
+        data = value;
+        next = nullptr;
+        prev = nullptr;
     }
     
 };
 
-class dequeue
+class Dequeue
+{
+    node *front, *rear;
+    public:
+    Dequeue()
     {
-        node *front,*rear;
-        public:
-        dequeue()
-        {
-            front =rear= Null;
-        }
+        front = rear = nullptr;
+    }
 
-        //push front
-        void push_front(int x)
+    //push front
+    void push_front(int x)
+    {
+        if(front == nullptr)
         {
-            if(front== null)
-            {
-                front = rear = new node(x);
-                return;
-            }
-            else
-            {
-                node *temp = new node(x);
-                temp->next =front;
-                front ->prev = temp;
-                front = temp;
-                return;
-            }
+            front = rear = new node(x);
+            return;
         }
+        else
+        {
+            node *temp = new node(x);
+            temp->next = front;
+            front->prev = temp;
+            front = temp;
+            return;
+        }
+    }
 
-        void push_back(int x)
+    void push_back(int x)
+    {
+        if(front == nullptr)
         {
-            if(front== null)
-            {
-                front = rear = new node(x);
-                return;
-            }
+            front = rear = new node(x);
+            return;
+        }
+        else
+        {
+            node *temp = new node(x);
+            rear->next = temp;
+            temp->prev = rear;
+            rear = temp;
+            return;
+        }
+    }
+    void pop_front()
+    {
+        if(front == nullptr)
+        {
+            cout << "Queue is empty";
+            return;
+        }
+        else
+        {
+            node *temp = front;
+            front = front->next;
+            if(front != nullptr)
+                front->prev = nullptr;
             else
-            {
-                node *temp = new node(x);
-                rear->next = temp;
-                temp->prev = rear;
-                rear=temp;
-                return;
-            }
+                rear = nullptr;
+            delete temp;
+        }
+    }
+    void pop_back()
+    {
+        if(front == nullptr)
+        {
+            cout << "Queue is empty";
+            return;
+        }
+        else
+        {
+            node *temp = rear;
+            rear = rear->prev;
+            if(rear != nullptr)
+                rear->next = nullptr;
+            else
+                front = nullptr;
+            delete temp;
+        }
+    }
 
-        }
-        void pop_front()
-        {
-            if(front== null)
-            {
-                cout<<"Queue is empty";
-                return;
-            }
-            else
-            {
-                node *temp = front;
-                front = front->next;
-                delete temp;
-            }
-        }
-        void pop_back()
-        {
-            if(front== null)
-            {
-                cout<<"Queue is empty";
-                return;
-            }
-            else
-            {
-                node *temp = rear;
-                rear = rear->prev;
-                delete temp;
-            }
-        }
-
-        int start()
-        {
-            if(front==null)
+    int start()
+    {
+        if(front == nullptr)
             return -1;
-            else 
+        else 
             return front->data;
-        }
-        int end()
-        {
-           if(front == null)
+    }
+    int end()
+    {
+       if(front == nullptr)
            return -1;
-           elsereturn rear ->data;
-        }
-    };
-
-
-
+       else
+           return rear->data;
+    }
+};
 
 int main(){
-    dequeue q;
+    Dequeue q;
     q.push_front(1);
     q.push_front(2);
     q.push_front(3);
     q.push_back(4);
     q.push_back(5);
 
-    
+    return 0;
 }
