ba1800ae9b10fcd48d1dfed553978c31d4fc8800
Rohan sinha <2003rohansinha@gmail.com>
2025-04-28 23:54:10 +0530

    Add Queue/dequeue_array.cpp

---
+++ Queue/dequeue_array.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+class dequeue
+{
+    int front , size, rear;
+    int *arr;
+
+    public:
+    
+    dequeue(int n)
+    {
+        size = n;
+        arr = new int[n];
+        front = -1;
+
+    }
+    void push_front()
+    
+};
+
+int main()
+{
+
+}
